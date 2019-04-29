c7e02ed225f672609cdc51de6733b7127ee4cb03
Melih Demircan <melih-demircan@hotmail.com>
2019-04-29 23:14:52 +0300

    add cloud plugin

---
+++ cloud/conf/metadata.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Metadata for configuration manager plugin
+ * Additions for the cloud plugin
+ *
+ * @author    Matthias Schulte <dokuwiki@lupo49.de>
+ */
+
+$meta['minimum_word_length']    = array('numeric', '_min' => 2);
+$meta['search_blacklist']       = array('string');
+$meta['word_blacklist']         = array('string');
+$meta['tag_blacklist']          = array('string');
+$meta['list_tags_of_subns']     = array('onoff');
+
+//Setup VIM: ex: et ts=2 :
---
+++ cloud/lang/fr/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author NicolasFriedli <nicolas@theologique.ch>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['minimum_word_length']   = 'Longueur minimale des mots dans le nuage';
+$lang['search_blacklist']      = 'Mots de recherche ne devant pas apparaître sur les pages (séparés par des virgules)';
+$lang['word_blacklist']        = 'Mots ne devant pas apparaître sur les pages (séparés par des virgules)';
+$lang['tag_blacklist']         = 'étiquettes ne devant pas apparaître sur les pages (séparées par des virgules)';
+$lang['list_tags_of_subns']    = 'Lister également les étiquettes des sous-catégories d\'une catégorie donnée';
